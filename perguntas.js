d7a128d8e03d019e56e83db48759ba051a0306f4
mariazinha-beep <00001077776810sp@al.educacao.sp.gov.br>
2024-11-13 08:42:09 -0300

    projeto FlashCards

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de informação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que execulta alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'como se diz Caixa de bombom em inglês?',
+    'Caixa de bombom em inglês é box of good good'
+)
+    
